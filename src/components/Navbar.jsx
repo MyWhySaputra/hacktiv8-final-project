322b6f0be912929fc2ee0ac55b5b0bdce5b6ad71
MyWahyuSap <wahyusaputra222000@gmail.com>
2024-10-02 20:14:55 +0700

    feat:update styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,7 +38,7 @@ export default function Navbar() {
             </ul>
           </div>
         </div>
-        <div className="navbar-center hidden lg:flex text-white">
+        <div className="navbar-center hidden lg:flex text-white ">
           <ul className="menu menu-horizontal menu-md px-1 gap-3">
             <li>
               <Link to="/">Home</Link>
