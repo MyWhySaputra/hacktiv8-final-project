46affcf01003687595ead2cb0c189ddec23e277c
MyWahyuSap <wahyusaputra222000@gmail.com>
2024-10-07 22:09:15 +0700

    feat:update styling

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -7,6 +7,7 @@ export default function LandingPage() {
         <h1 className="text-3xl font-bold text-center mb-6">
           Country Population Ranking
         </h1>
+        
         <CountryList/>
       </div>
     </div>
